53269b4148c30abe3b93762bb6a6167a55fcb246
abhishek <abhi3vish@gmail.com>
2025-01-07 13:13:35 +0530

    test deployment

--- Backend/Middleware/authUser.js
+++ Backend/Middleware/authUser.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const authUser=async(req,res,next)=>{
     try {
-        const {token}=req.headers;
+        const {token}=req.C
         // console.log(token);
         
         if(!token){
